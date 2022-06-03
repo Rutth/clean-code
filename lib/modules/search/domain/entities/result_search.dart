652d2ae681ab5c58b8ede44a4adc68299fe9df04
Ruth <ruth.barros12@gmail.com>
2022-06-02 22:02:03 -0300

    Modules, repo e testes

---
+++ lib/modules/search/domain/entities/result_search.dart
@@ -0,0 +1,7 @@
+class ResultSearch {
+  final String title;
+  final String img;
+  final String content;
+
+  ResultSearch({required this.title, required this.img, required this.content});
+}
---
+++ test/modules/search/domain/usecases/search_by_text_test.dart
@@ -0,0 +1,27 @@
+import 'package:clean_code/modules/search/domain/entities/result_search.dart';
+import 'package:clean_code/modules/search/domain/errors/errors.dart';
+import 'package:clean_code/modules/search/domain/repositories/search_repository.dart';
+import 'package:clean_code/modules/search/domain/usecases/search_by_text.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'search_by_text_test.mocks.dart';
+
+// class SearchRepositoryMock extends Mock implements SearchRepository {}
+@GenerateMocks([SearchRepository])
+main() {
+  final repository = MockSearchRepository();
+  final usecase = SearchByTextImpl(repository);
+
+  test('deve retornar uma lista de ResultSearch', () async {
+    when(repository.search(any))
+        .thenAnswer((_) async => const Right(<ResultSearch>[]));
+
+    final result = await usecase("ruth");
+
+    // expect(result, isA<Right>());
+    expect(result.getOrElse(() => null), isA<List<ResultSearch>>());
+  });
+}
